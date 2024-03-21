9d171ecee9f6cca560d7d70eda031d5d6173e528
rishi <rishimakwana.synsoft@gmail.com>
2024-03-21 12:34:17 +0530

    updte

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ app.get('/', (req, res) => {
     res.send(`Welcome to our Express API! Thank you for visiting. We aim to provide you with a seamless experience. Feel free to explore our endpoints and interact with our services. If you have any questions or need assistance, don't hesitate to reach out. Have a fantastic day ahead!`);
 });
 
+
+
+
 const users = [
-    { id: 1, name: 'John Doe', email: 'john@example.com' },
-    { id: 2, name: 'Jane Smith', email: 'jane@example.com' },
-    { id: 3, name: 'Alice Johnson', email: 'alice@example.com' },
+    { id: 1, name: 'John Doe', email: 'john@gmail.com' },
+    { id: 2, name: 'Jane Smith', email: 'jane@gmail.com' },
+    { id: 3, name: 'Alice Johnson', email: 'alice@gmail.com' },
 ];
 
 // GET /users - Get all users
